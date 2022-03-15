964752b2038ddf9f6d5cbba1eb455f4b59971e53
hyejun <hyejun0608@naver.com>
2022-03-15 19:57:57 +0900

    [INIT]

---
+++ bookstoreinmemory/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { RecoilRoot } from 'recoil';
+import App from './App';
+
+ReactDom.render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
+  document.getElementById('root'),
+);
