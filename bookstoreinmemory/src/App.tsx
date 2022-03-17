4e37166ae0fc4fa6a9ce8e5dbe4fc20ec48b210a
hyejun <hyejun0608@naver.com>
2022-03-17 19:51:28 +0900

    [ADD] App

---
+++ bookstoreinmemory/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyle from './GlobalStyle';
+import RootRouter from './router/RootRouter';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <RootRouter />
+    </>
+  );
+}
+
+export default App;
