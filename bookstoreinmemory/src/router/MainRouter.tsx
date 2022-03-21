18e0c03d625796bc59bb296812da338cdc51243d
hyejun <hyejun0608@naver.com>
2022-03-21 11:20:45 +0900

    [ADD] record component 추가

--- bookstoreinmemory/src/router/MainRouter.tsx
+++ bookstoreinmemory/src/router/MainRouter.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Auth, Feed } from '../components';
+import { Auth, Feed, Record } from '../components';
 
 const MainRouter = () => {
   return (
@@ -9,6 +9,7 @@ const MainRouter = () => {
         <Route path='/signin' element={<Auth isSignup={false} />} />
         <Route path='/signup' element={<Auth isSignup={true} />} />
         <Route path='/feed' element={<Feed />} />
+        <Route path='/record' element={<Record />} />
       </Routes>
     </>
   );
