20350bf1c59cea24487734e62fa6d5f471a630d7
hyejun <hyejun0608@naver.com>
2022-04-11 10:00:23 +0900

    [FIX] props로 값 전달 & 받음

--- bookstoreinmemory/src/components/feed/Feed.tsx
+++ bookstoreinmemory/src/components/feed/Feed.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { FC } from 'react';
 import * as S from './style';
 import Header from '../header/Header';
 import Content from './Content';
 
-const Feed = () => {
-  const userName = '혜준';
+interface Props {
+  userName: string;
+}
+
+const Feed: FC<Props> = props => {
+  const { userName } = props;
   return (
     <S.Feed>
       <Header />
-      <S.FeedTitle>{userName}님의 기억 속 책들</S.FeedTitle>
-      <Content />
+      <S.FeedTitle>
+        <span>{userName}</span>님의 기억 속 책들
+      </S.FeedTitle>
+      <Content
+        title={'처음부터 배우는 딥러닝 챗봇처음부터 배우는 딥러닝 챗봇'}
+        author={'저자'}
+        count={1}
+      />
     </S.Feed>
   );
 };
