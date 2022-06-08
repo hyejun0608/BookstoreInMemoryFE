ff18236274a9c77b316312a7b4e581339edbb847
hyejun <hyejun0608@naver.com>
2022-06-08 12:02:42 +0900

    [ADD] signin state 생성 & onChange 함수

--- bookstoreinmemory/src/components/auth/Signin.tsx
+++ bookstoreinmemory/src/components/auth/Signin.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as S from './style';
 
 const Signin = () => {
+  const [id, setId] = useState<string>('');
+  const [password, setPassword] = useState<String>('');
   const navigate = useNavigate();
   const signupBtnClickHandler = () => {
     navigate('/signup');
+  };
+
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.name === 'id') setId(event.currentTarget.value);
+    else if (event.currentTarget.name === 'password') setPassword(event.currentTarget.value);
   };
 
   return (
@@ -15,8 +22,13 @@ const Signin = () => {
           <S.AuthTitle>Signin</S.AuthTitle>
           <S.TitleLine width={77} />
         </S.AuthTitleBox>
-        <S.Input placeholder={'아이디를 입력하세요...'} />
-        <S.Input type={'password'} placeholder={'비밀번호를 입력하세요...'} />
+        <S.Input placeholder={'아이디를 입력하세요...'} name={'id'} onChange={inputChangeHandler} />
+        <S.Input
+          type={'password'}
+          placeholder={'비밀번호를 입력하세요...'}
+          name={'password'}
+          onChange={inputChangeHandler}
+        />
         <S.Button>
           <b>Signin</b>
         </S.Button>
