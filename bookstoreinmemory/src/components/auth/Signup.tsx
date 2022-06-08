d56a29a0eaab4f519d7607a51b89aec6d0963000
hyejun <hyejun0608@naver.com>
2022-06-08 12:02:21 +0900

    [ADD] signup state 생성 & onChange 함수

--- bookstoreinmemory/src/components/auth/Signup.tsx
+++ bookstoreinmemory/src/components/auth/Signup.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as S from './style';
 
 const Signup = () => {
+  const [name, setName] = useState<string>('');
+  const [id, setId] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   const navigate = useNavigate();
   const signinBtnClickHandler = () => {
     navigate('/signin');
+  };
+
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.name === 'name') setName(event.currentTarget.value);
+    else if (event.currentTarget.name === 'id') setId(event.currentTarget.value);
+    else if (event.currentTarget.name === 'password') setPassword(event.currentTarget.value);
   };
 
   return (
@@ -15,9 +25,14 @@ const Signup = () => {
           <S.AuthTitle>Signup</S.AuthTitle>
           <S.TitleLine width={86} />
         </S.AuthTitleBox>
-        <S.Input placeholder={'이름을 입력하세요...'} />
-        <S.Input placeholder={'아이디를 입력하세요...'} />
-        <S.Input type={'password'} placeholder={'비밀번호를 입력하세요...'} />
+        <S.Input placeholder={'이름을 입력하세요...'} name={'name'} onChange={inputChangeHandler} />
+        <S.Input placeholder={'아이디를 입력하세요...'} name={'id'} onChange={inputChangeHandler} />
+        <S.Input
+          type={'password'}
+          placeholder={'비밀번호를 입력하세요...'}
+          name={'password'}
+          onChange={inputChangeHandler}
+        />
         <S.Button>
           <b>Signup</b>
         </S.Button>
