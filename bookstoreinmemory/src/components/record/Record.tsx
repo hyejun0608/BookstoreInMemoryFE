2328ea295b1af3228bcf0d2cef5837d9d8387589
hyejun <hyejun0608@naver.com>
2022-06-08 12:02:00 +0900

    [ADD] record state 생성

--- bookstoreinmemory/src/components/record/Record.tsx
+++ bookstoreinmemory/src/components/record/Record.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
 import Header from '../header/Header';
 
 const Record = () => {
+  const [title, setTitle] = useState<string>('');
+  const [author, setAuthor] = useState<string>('');
+  const [count, setCount] = useState<number>(0);
+  const [content, setContent] = useState<string>('');
+
+  const titleChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value);
+  };
+  const authorChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuthor(event.currentTarget.value);
+  };
+  const countChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCount(Number(event.currentTarget.value));
+  };
+  const contentChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.currentTarget.value);
+  };
+
   return (
     <S.Record>
       <Header />
@@ -12,17 +30,26 @@ const Record = () => {
         </S.RecordTitle>
         <S.RecordContent>
           <div>
-            <S.Input width={100} placeholder={'책 제목을 입력하세요...'} />
+            <S.Input
+              width={100}
+              placeholder={'책 제목을 입력하세요...'}
+              onChange={titleChangeHandler}
+            />
             <div>
-              <S.Input width={470} placeholder={'책 저자를 입력하세요...'} />
+              <S.Input
+                width={470}
+                placeholder={'책 저자를 입력하세요...'}
+                onChange={authorChangeHandler}
+              />
               <S.Input
                 width={155}
                 type='number'
                 placeholder={'독서 횟수를 입력하세요...'}
                 min={0}
+                onChange={countChangeHandler}
               />
             </div>
-            <S.TextArea placeholder={'책 내용을 입력하세요...'} />
+            <S.TextArea placeholder={'책 내용을 입력하세요...'} onChange={contentChangeHandler} />
             <S.RecordBtn>등록하기</S.RecordBtn>
           </div>
         </S.RecordContent>
